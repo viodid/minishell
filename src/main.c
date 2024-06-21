b2b305172456893a6c31516ee44efae7e3552a17
kde-la-c <kdelacolinaplant@gmail.com>
2024-06-21 04:01:49 +0200

    fixed segfault while separating envp values in key/value pairs inside set_env

--- src/main.c
+++ src/main.c
@@ -12,55 +12,26 @@
 
 #include "../include/minishell.h"
 
-char	*ft_getenv(char *key, char **envp)
-{
-	int		i;
-	int		keylen;
-
-	if (!envp)
-		return (NULL);
-	key = ft_strjoin(key, "=");
-	keylen = ft_strlen(key);
-	i = -1;
-	while (envp[++i])
-	{
-		if (!ft_strncmp(key, envp[i], keylen))
-			return (free(key), ft_strchr(envp[i], '=') + 1);
-	}
-	return (free(key), NULL);
-}
-
-char	*get_key(char *var)
-{
-	int		i;
-	char	*key;
-
-	i = ft_strchr(var, '=') - var;
-	var[i] = 0;
-	key = ft_strdup(var);
-	return (key);
-}
-
 t_list	*set_env(char **envp)
 {
 	int		i;
-	char	*tmp;
+	char	*tmpenv;
 	t_var	var;
 	t_list	*ret;
 
 	i = -1;
-	ret = ft_lstnew(NULL);
+	ret = NULL;
 	while (envp[++i])
 	{
-		var.key = get_key(envp[i]);
-		printf("%s=", var.key);
-		tmp = ft_strchr(envp[i], '=') + 1;
-		//TODO solve value attribution problem for values
-		if (tmp)
-			var.value = ft_strdup(tmp);
-		else
-			var.value = NULL;
-		printf("%s\n", var.value);
+		tmpenv = ft_strdup(envp[i]);
+		var.key = ft_substr(tmpenv, 0, ft_strchr(tmpenv, '=') - tmpenv);
+		free(tmpenv);
+		tmpenv = ft_strdup(envp[i]);
+		var.value = ft_strdup(ft_strchr(tmpenv, '=') + 1);
+		free(tmpenv);
+
+
+		printf("%s=%s\n", var.key, var.value);
 	}
 	return (ret);
 }
