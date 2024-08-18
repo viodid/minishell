a39d6372af8845190c5c1e70d3a015077192df3d
viodid <david.yunta.aller@gmail.com>
2024-08-18 21:38:35 +0200

    lexer func def

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,9 +6,16 @@
 /*   By: dyunta <dyunta@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 18:12:28 by dyunta            #+#    #+#             */
-/*   Updated: 2024/08/17 18:12:40 by dyunta           ###   ########.fr       */
+/*   Updated: 2024/08/18 21:36:08 by dyunta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "../../include/minishell.h"
+
+t_list	*lexer(void)
+{
+	char	*user_input;
+
+	user_input = readline("minishell >");
+}
