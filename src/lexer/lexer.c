1f2c6e29bbd22586f7a0d6e4ed4f291cc9a3463d
viodid <david.yunta.aller@gmail.com>
2024-08-19 19:55:18 +0200

    construct full cmd if multiline command is typed wip

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: dyunta <dyunta@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 18:12:28 by dyunta            #+#    #+#             */
-/*   Updated: 2024/08/19 19:15:07 by dyunta           ###   ########.fr       */
+/*   Updated: 2024/08/19 19:53:06 by dyunta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 //t_list	*lexer(void)
 //{
+//	const char	*metacharacters = " \t\n\\|&;()<>";
 //	char	*user_input;
 //
 //	user_input = readline("minishell >");
@@ -22,14 +23,17 @@
 
 char	*join_all_commands(void)
 {
-	const char* metacharacters = " \t\n\\|&;()<>";
-	char*	output_command;
-	char*	user_input;
-	char*	tmp_str;
+	char		*output_command;
+	char		*user_input;
+	char		*tmp_str;
 
 	output_command = NULL;
-	tmp_str = colored_output("minishell >", BLUE);
-	user_input = readline("\\033[0;31mminishell >\\033[0;33m");
-
+	user_input = readline("minishell >");
+	tmp_str = ft_strtrim(user_input, " ");
+	output_command = ft_strdup(tmp_str);
+	free(user_input);
+	while (output_command[ft_strlen(output_command) - 1] == '\\')
+		output_command = ft_strjoin_f12(output_command, readline("minishell >"));
+	free(tmp_str);
 	return (output_command);
 }
