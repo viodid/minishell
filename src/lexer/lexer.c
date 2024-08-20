ec397b179f9c3687cedd100c37951977311f0a00
David Yunta <david.yunta.aller@gmail.com>
2024-08-20 22:21:23 +0200

    lexer split str wip

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -14,17 +14,38 @@
 #include "../../include/minishell.h"
 
 static char	*loop_readline(const char metachar);
+t_list	*tokenizer(const char *user_input);
 
 t_list	*lexer(void)
 {
-	// const char	*metacharacters = " \t\n\\|&;()<>";
 	char	*user_input;
 	t_list	*token_list;
 
 	user_input = loop_readline('\\');
 	if (!*user_input)
 		return (EXIT_SUCCESS);
+	token_list = tokenizer(user_input);
 	return (token_list);
+}
+
+t_list	*tokenizer(const char *user_input)
+{
+	const char	*metacharacters = " \t\n|&;()<>";
+	t_list		*token_list;
+	t_token		*token;
+	uint32_t	i;
+
+	i = 0;
+	while (user_input[i])
+	{
+		if (ft_strchr(metacharacters, user_input[i]))
+		{
+			token = (t_token *)malloc(sizeof(t_token));
+			token->value = (char *)TODO;
+			token_list = ft_lstnew();
+		}
+		i++;
+	}
 }
 
 static char	*loop_readline(const char metachar)
@@ -35,14 +56,12 @@ static char	*loop_readline(const char metachar)
 	tmp_str1 = NULL;
 	tmp_str2 = NULL;
 	tmp_str1 = readline("minishell >");
-	while (TRUE)
+	while (tmp_str1[ft_strlen(tmp_str1) - 1] != metachar)
 	{
-		tmp_str2 = ft_strtrim(tmp_str1, " ");
+		tmp_str2 = ft_strtrim(tmp_str1, " \t\n");
 		free(tmp_str1);
-		if (tmp_str2[ft_strlen(tmp_str2) - 1] != metachar)
-			break ;
 		tmp_str1 = ft_strjoin_f12(tmp_str2, readline(">"));
 	}
-	return (tmp_str2);
+	return (tmp_str1);
 }
 
