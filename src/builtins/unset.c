5869adf134ac45bff7481ce597ecb44d40813fc2
kde-la-c <kdelacolinaplant@gmail.com>
2024-08-27 21:01:09 +0200

    Revert "changed builtins parameters to fit the functions best"
    This reverts commit 89a22ddc67dbc759c47f5e99b25cc2858d933548.

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -3,23 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kde-la-c <kde-la-c@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kde-la-c <kde-la-c@student.42Madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/22 17:07:48 by kde-la-c          #+#    #+#             */
-/*   Updated: 2024/08/26 20:20:30 by kde-la-c         ###   ########.fr       */
+/*   Updated: 2024/06/22 17:07:48 by kde-la-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-// int	ft_unset(t_data *core, char *key)
-int	ft_unset(t_list *env, char *key)
+// int	ft_unset(t_list *env, char **args)
+int	ft_unset(t_data *core, char *key)
 {
 	t_list	*tmp;
 	t_var	*var;
 
-	tmp = env;
-	var = get_env(env, key);
+	tmp = core->env;
+	var = get_env(core, key);
 	while (tmp && tmp->next)
 	{
 		if (tmp->next->content == var)
--- src/core/get_env.c
+++ src/core/get_env.c
@@ -3,21 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   get_env.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kde-la-c <kde-la-c@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kde-la-c <kde-la-c@student.42Madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 18:54:21 by kde-la-c          #+#    #+#             */
-/*   Updated: 2024/08/26 20:07:55 by kde-la-c         ###   ########.fr       */
+/*   Updated: 2024/06/09 18:54:24 by kde-la-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-t_var	*get_env(t_list *env, char *key)
+t_var	*get_env(t_data *core, char *key)
 {
 	int		keylen;
 	t_list	*tmp;
 
-	tmp = env;
+	tmp = core->env;
 	keylen = ft_strlen(key);
 	while (tmp)
 	{
