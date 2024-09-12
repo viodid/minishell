a5baa023472220d0b034486b720673a1afcbf478
viodid <david.yunta.aller@gmail.com>
2024-09-12 20:59:42 +0200

    change and replace line. line->

--- src/executor/set_fds.c
+++ src/executor/set_fds.c
@@ -15,14 +15,14 @@
 int	close_fds(t_data *core, t_pipe_fds fds)
 {
 	if (fds == READ_FD)
-		return (close(core->line.fds[READ_FD]));
+		return (close(core->line->fds[READ_FD]));
 	else if (fds == WRITE_FD)
-		return (close(core->line.fds[WRITE_FD]));
+		return (close(core->line->fds[WRITE_FD]));
 	else if (fds == BOTH_FDS)
 	{
-		if (close(core->line.fds[READ_FD]))
+		if (close(core->line->fds[READ_FD]))
 			return (errno);
-		if (close(core->line.fds[WRITE_FD]))
+		if (close(core->line->fds[WRITE_FD]))
 			return (errno);
 	}
 	return (EXIT_SUCCESS);
@@ -30,27 +30,27 @@ int	close_fds(t_data *core, t_pipe_fds fds)
 
 int	set_fds(t_fds *fds, t_data *core, int cmd_nb)
 {
-	if (pipe(core->line.fds))
+	if (pipe(core->line->fds))
 		return (perror("piping"), errno);
-	if (core->line.nbcommands == 1)
+	if (core->line->nbcommands == 1)
 	{
 		fds->stdfdin = STDIN_FILENO;
 		fds->stdfdout = STDOUT_FILENO;		
 	}
-	else if (cmd_nb == 0 && core->line.nbcommands > 1)
+	else if (cmd_nb == 0 && core->line->nbcommands > 1)
 	{
 		fds->stdfdin = STDIN_FILENO;
-		fds->stdfdout = core->line.fds[WRITE_FD];
+		fds->stdfdout = core->line->fds[WRITE_FD];
 	}
-	else if (cmd_nb && cmd_nb == core->line.nbcommands - 1)
+	else if (cmd_nb && cmd_nb == core->line->nbcommands - 1)
 	{
-		fds->stdfdin = core->line.fds[READ_FD];
+		fds->stdfdin = core->line->fds[READ_FD];
 		fds->stdfdout = STDOUT_FILENO;
 	}
 	else
 	{
-		fds->stdfdin = core->line.fds[READ_FD];
-		fds->stdfdout = core->line.fds[WRITE_FD];
+		fds->stdfdin = core->line->fds[READ_FD];
+		fds->stdfdout = core->line->fds[WRITE_FD];
 	}
 	return (EXIT_SUCCESS);
 }
