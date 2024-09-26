8a1129a8f15b991d31b55a8ac668c6445785cee7
David Yunta <david.yunta.aller@gmail.com>
2024-09-27 00:18:38 +0200

    sync parser and executor

--- src/core/minishell.c
+++ src/core/minishell.c
@@ -6,44 +6,23 @@
 /*   By: kde-la-c <kde-la-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 18:27:07 by kde-la-c          #+#    #+#             */
-/*   Updated: 2024/09/10 20:17:17 by dyunta           ###   ########.fr       */
+/*   Updated: 2024/09/27 00:02:15 by dyunta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-// int	minishell(t_data *core)
-// {
-// 	int			retcode;
-// 	char		**cmds;
+ int	minishell(t_data *core)
+ {
+ 	int			retcode;
+ 	char		**cmds;
 
-// 	errno = 0;
-// 	parser(core);
-// 	if (errno)
-// 		return(EXIT_SUCCESS);
-// 	retcode = executor(core);
-// 	retcode = 0;
-// 	return (retcode);
-// }
-
-int	minishell(t_data *core)
-{
-	int			retcode;
-	char		*str;
-	char		**cmds;
-
-	str = readline("ˢʰᵉˡˡ$>");
-	cmds = ft_split(str, '|');
-	if (!cmds)
-		return (EXIT_SUCCESS);
-	if (core->line && core->line->cmds)
-		ft_lstclear(&core->line->cmds, free_cmd);
-	tmp_parser(core, cmds);
-
-	retcode = executor(core);
-	if (ft_strlen(str))
-		add_history(str);
-	free(str);
-	ft_dfree((void **)cmds);
-	return (retcode);
+ 	errno = 0;
+ 	parser(core);
+ 	if (errno)
+ 		return(EXIT_SUCCESS);
+ 	retcode = executor(core);
+ 	retcode = 0;
+ 	return (retcode);
 }
+
--- src/parser/lexer.c
+++ src/parser/lexer.c
@@ -6,7 +6,7 @@
 /*   By: dyunta <dyunta@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 18:12:28 by dyunta            #+#    #+#             */
-/*   Updated: 2024/09/26 22:17:02 by dyunta           ###   ########.fr       */
+/*   Updated: 2024/09/27 00:15:35 by dyunta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -106,7 +106,9 @@ static char	*loop_readline(void)
 
 	tmp_str1 = NULL;
 	tmp_str2 = NULL;
-	tmp_str1 = readline("ˢʰᵉˡˡ >");
+	tmp_str1 = readline("ˢʰᵉˡˡ > ");
+    if (ft_strlen(tmp_str1) == 0)
+        return (tmp_str1);
 	while (tmp_str1[ft_strlen(tmp_str1) - 1] == '\\')
 	{
 		tmp_str2 = ft_strtrim(tmp_str1, "\\");
