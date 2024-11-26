db039276d8a00243a5983212599fc73989185a4d
kde-la-c <kdelacolinaplant@gmail.com>
2024-11-26 20:51:35 +0100

    refactored var name

--- include/minishell.h
+++ include/minishell.h
@@ -33,7 +33,7 @@
 # include <sys/wait.h>
 # include <signal.h>
 
-extern volatile sig_atomic_t signum;
+extern volatile sig_atomic_t g_signum;
 
 /*================================== ENUMS ===================================*/
 
