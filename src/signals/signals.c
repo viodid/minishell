77c8d0d56102a96e3077059fa65f8b68dc1e15c1
viodid <david.yunta.aller@gmail.com>
2024-12-02 18:12:55 +0100

    change signum parameter name

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,29 +6,26 @@
 /*   By: dyunta <dyunta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 18:22:43 by kde-la-c          #+#    #+#             */
-/*   Updated: 2024/11/26 20:20:50 by dyunta           ###   ########.fr       */
+/*   Updated: 2024/11/29 19:59:36 by dyunta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-static void	handle_interactive_signals(int g_signum)
+static void	handle_interactive_signals(int signum)
 {
 	write(STDOUT_FILENO, "\n", 1);
 	rl_replace_line("", FALSE);
 	rl_on_new_line();
 	rl_redisplay();
-	(void) g_signum;
+	(void) signum;
 }
 
-static void	handle_non_interactive_signals(int g_signum)
+static void	handle_non_interactive_signals(int signum)
 {
-	if (g_signum == SIGINT)
-	{
+	if (signum == SIGINT)
 		ft_putendl_fd("\n", 2);
-		g_signum = 130;
-	}
-	else if (g_signum == SIGQUIT)
+	else if (signum == SIGQUIT)
 		ft_putendl_fd("Quit\n", 5);
 }
 
